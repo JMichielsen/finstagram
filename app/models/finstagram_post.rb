6ce9a2dadc815ecd4a89e6999b8179009ce10dd4
Jack Michielsen <jack.michielsen@gmail.com>
2021-04-27 02:32:18 +0000

    adding W4D1 code

--- app/models/finstagram_post.rb
+++ app/models/finstagram_post.rb
@@ -1,3 +1,31 @@
 class FinstagramPost < ActiveRecord::Base
 
+    belongs_to :user
+    has_many :comments
+    has_many :likes
+
+    validates_presence_of :user
+
+    # Calculates the age of the post in hours
+    def humanized_time_ago
+        time_ago_in_seconds = Time.now - self.created_at
+        time_ago_in_minutes = time_ago_in_seconds / 60
+
+        if time_ago_in_minutes >= 60
+            "#{(time_ago_in_minutes / 60).to_i} hours ago."
+        else
+            "#{time_ago_in_minutes.to_i} minutes ago."
+        end
+    end
+
+    # Calculates likes
+    def like_count
+        self.likes.size
+    end
+
+    # Calculates comment count
+    def comment_count
+        self.comments.size
+    end
+
 end
