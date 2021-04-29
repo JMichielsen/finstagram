1a001dd730fb98425b8b2025a564ff5b5201a0b3
Jack Michielsen <jack.michielsen@gmail.com>
2021-04-29 02:44:05 +0000

    added W4D2 modifications

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,32 @@
-get '/' do
-    
+get '/' do              # Main Page
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
+end
+
+get '/signup' do
+    # setup an empty @user object
+    @user = User.new
+
+    # render "app/views/signup.erb"
+    erb(:signup)                
+end
+
+
+post '/signup' do       # Sign-up Page
+    # grab the user input values from params
+    email       = params[:email]
+    avatar_url  = params[:avatar_url]
+    username    = params[:username]
+    password    = params[:password]
+
+    # instantiate and save the new User
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+    
+    # if user validation succeeded in user.rb, the user can be saved.
+    if @user.save # = true
+        "User #{username} saved!"
+    else
+        # display error cause & let them try again
+        erb(:signup)
+    end
 end
