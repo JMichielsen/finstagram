8d820f8c90e263b75b4646155064e839b94cf637
Jack Michielsen <jack.michielsen@gmail.com>
2021-04-29 03:32:55 +0000

    W4D2 - add model validation and bcrypt password hashing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
+
+    has_secure_password # bcrypt hashes password
     
     has_many :finstagram_posts
     has_many :comments
     has_many :likes
 
     validates :email, :username, uniqueness: true
-    validates :email, :avatar_url, :username, :password, presence: true
+    validates :email, :avatar_url, :username, presence: true
 end
