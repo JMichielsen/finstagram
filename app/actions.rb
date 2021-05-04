e15a208e82d486b75d6864a9ef3d233d3b0e4b7c
Jack Michielsen <jack.michielsen@gmail.com>
2021-05-04 02:11:39 +0000

    added login functionality (W5D1)

--- app/actions.rb
+++ app/actions.rb
@@ -1,9 +1,15 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do              # Main Page
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
 end
 
-get '/signup' do
+get '/signup' do        # Sign-up Page (blank)
     # setup an empty @user object to prevent signup.erb from erroring out trying to find error messages.
     @user = User.new
 
@@ -12,7 +18,7 @@ get '/signup' do
 end
 
 
-post '/signup' do       # Sign-up Page
+post '/signup' do       # Sign-up Page (submit)
     # grab the user input values from params
     email       = params[:email]
     avatar_url  = params[:avatar_url]
@@ -24,9 +30,40 @@ post '/signup' do       # Sign-up Page
     
     # if user validation succeeded in user.rb, the user can be saved.
     if @user.save # = true
-        "User #{username} saved!"
+        redirect to ('/login')
     else
         # display error cause & let them try again
         erb(:signup)
     end
+end
+
+get '/login' do         # Log in page (Blank)
+    erb(:login)
+end
+
+post '/login' do        # Log in page (submit)
+    username = params[:username]
+    password = params[:password]
+
+    # 1 find user by username
+    @user = User.find_by(username: username)
+
+    # 2 if that user exists
+    if @user
+        # and their password matches (using bCrypt method)
+        if @user.authenticate(password)
+            session[:user_id] = @user.id
+            redirect to ('/')
+        else
+            @error_message = "Login Failed, please check if your password is correct."
+            erb(:login)
+        end
+    else
+        "User does not exist"
+    end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to ('/')
 end
